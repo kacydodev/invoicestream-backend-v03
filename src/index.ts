af5b9f84c0fc65b34ea8784444499e0d06341677
Kacy Do <contact@kacydo.dev>
2025-11-08 14:39:56 +1100

    add morgan middleware

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes';
+import morgan from 'morgan';
 
 dotenv.config();
 const port = process.env.PORT;
 
 const app = express();
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/', router);
